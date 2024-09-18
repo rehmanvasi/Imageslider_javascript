49ad713537c389407213341275217c9a9178d661
rehmanvasi <rehmanvasi@gmail.com>
2024-09-17 22:48:00 -0500

    Image_slider

---
+++ index.js
@@ -0,0 +1,33 @@
+const slides= document.querySelectorAll(".slides img");
+let slideIndex=0;
+let intervalId=null;
+
+//intializeSlider();
+document.addEventListener("DOMContentLoaded",intializeSlider);
+function intializeSlider(){
+
+    if(slides.length>0){
+    slides[slideIndex].classList.add("displaySlide");
+   //intervalId= setInterval(nextSlide,5000);
+}
+}
+function showSlide(index){
+    if (index >= slides.length) {
+        slideIndex=0;
+    } else if(index<0) {
+        slideIndex=slides.length-1;
+    }5
+slides.forEach(slide=>{
+slide.classList.remove("displaySlide");
+});
+slides[slideIndex].classList.add("displaySlide");
+}
+function prevSlide(){
+    clearInterval(intervalId);
+    slideIndex--;
+    showSlide(slideIndex);
+}
+function nextSlide(){
+    slideIndex++;
+    showSlide(slideIndex);
+}
